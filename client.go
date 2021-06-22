40e284873292717d44c44df0a13674eacda925c0
akez <1791189210@qq.com>
2021-06-22 15:40:06 +0800

    gorilla websocket

---
+++ client.go
@@ -0,0 +1,34 @@
+package main
+
+import (
+	"github.com/gorilla/websocket"
+	"time"
+)
+
+const (
+	writeWait      = 10 * time.Second
+	pongWait       = 60 * time.Second
+	pingPeriod     = (pongWait * 9) / 10
+	maxMessageSize = 512
+)
+
+var (
+	newLine = []byte{'\n'}
+	space   = []byte{' '}
+)
+
+var upgrade = websocket.Upgrader{
+	ReadBufferSize:  1024,
+	WriteBufferSize: 1024,
+}
+
+type Client struct {
+	hub  *Hub
+	conn *websocket.Conn
+	send chan []byte
+}
+
+func (c *Client) readPump() {
+	defer func() {
+	}()
+}
---
+++ hub.go
@@ -0,0 +1,27 @@
+package main
+
+type Hub struct {
+	clients    map[*Client]bool
+	broadcast  chan []byte
+	register   chan *Client
+	unregister chan *Client
+}
+
+func newHub() *Hub {
+	return &Hub{
+		clients:    make(map[*Client]bool),
+		broadcast:  make(chan []byte),
+		register:   make(chan *Client),
+		unregister: make(chan *Client),
+	}
+}
+
+func (h *Hub) run() {
+	for {
+		select {
+		case client := <-h.register:
+			h.clients[client] = true
+
+		}
+	}
+}
