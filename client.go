488db920f7d6a16f5aeb58289d8097d14a8579fa
akez <1791189210@qq.com>
2021-06-22 17:20:51 +0800

    complete websocket chat room

--- client.go
+++ client.go
@@ -1,7 +1,10 @@
 package main
 
 import (
+	"bytes"
 	"github.com/gorilla/websocket"
+	"log"
+	"net/http"
 	"time"
 )
 
@@ -13,7 +16,7 @@ const (
 )
 
 var (
-	newLine = []byte{'\n'}
+	newline = []byte{'\n'}
 	space   = []byte{' '}
 )
 
@@ -30,5 +33,84 @@ type Client struct {
 
 func (c *Client) readPump() {
 	defer func() {
+		c.hub.unregister <- c
+		_ = c.conn.Close()
 	}()
+	c.conn.SetReadLimit(maxMessageSize)
+	_ = c.conn.SetReadDeadline(time.Now().Add(pongWait))
+	c.conn.SetPongHandler(func(appData string) error {
+		err := c.conn.SetReadDeadline(time.Now().Add(pongWait))
+		if err != nil {
+			return err
+		}
+		return nil
+	})
+
+	for {
+		_, message, err := c.conn.ReadMessage()
+		if err != nil {
+			if websocket.IsUnexpectedCloseError(err, websocket.CloseGoingAway, websocket.CloseAbnormalClosure) {
+				log.Printf("error: %v", err)
+			}
+			break
+		}
+		message = bytes.TrimSpace(bytes.Replace(message, newline, space, -1))
+		c.hub.broadcast <- message
+	}
+}
+
+func (c *Client) writePump() {
+	ticker := time.NewTicker(pingPeriod)
+	defer func() {
+		ticker.Stop()
+		_ = c.conn.Close()
+	}()
+	for {
+		select {
+		case message, ok := <-c.send:
+			_ = c.conn.SetWriteDeadline(time.Now().Add(writeWait))
+			if !ok {
+				_ = c.conn.WriteMessage(websocket.CloseMessage, []byte{})
+				return
+			}
+
+			w, err := c.conn.NextWriter(websocket.TextMessage)
+			if err != nil {
+				return
+			}
+			_, _ = w.Write(message)
+
+			n := len(c.send)
+			for i := 0; i < n; i++ {
+				_, _ = w.Write(newline)
+				_, _ = w.Write(<-c.send)
+			}
+
+			if err := w.Close(); err != nil {
+				return
+			}
+		case <-ticker.C:
+			_ = c.conn.SetWriteDeadline(time.Now().Add(writeWait))
+			if err := c.conn.WriteMessage(websocket.PingMessage, nil); err != nil {
+				return
+			}
+		}
+	}
+}
+
+func serveWs(hub *Hub, w http.ResponseWriter, r *http.Request) {
+	coon, err := upgrade.Upgrade(w, r, nil)
+	if err != nil {
+		log.Println(err)
+		return
+	}
+	client := &Client{
+		hub:  hub,
+		conn: coon,
+		send: make(chan []byte, 256),
+	}
+	client.hub.register <- client
+
+	go client.writePump()
+	go client.readPump()
 }
